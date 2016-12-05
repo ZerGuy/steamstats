79d0804e1c4129ca0f4f75fa2b8a9323754cde77
Sergei Olenev <sergei_olenev@epam.com>
2016-12-06 00:06:58 +0300

    new project with maven

---
+++ src/main/java/ru/zerguy/steamstats/Main.java
@@ -0,0 +1,52 @@
+package ru.zerguy.steamstats;
+
+import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.transport.InetSocketTransportAddress;
+import org.elasticsearch.common.xcontent.XContentBuilder;
+import org.elasticsearch.transport.client.PreBuiltTransportClient;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.elasticsearch.common.xcontent.XContentFactory.jsonBuilder;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        TransportClient client = new PreBuiltTransportClient(Settings.EMPTY)
+                .addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName("localhost"), 9300));
+
+
+        Map<String, Object> json = new HashMap<String, Object>();
+        json.put("user","kimchy");
+        json.put("postDate",new Date());
+        json.put("message","trying out Elasticsearch");
+
+
+        XContentBuilder builder = jsonBuilder()
+                .startObject()
+                .field("user", "kimchy")
+                .field("postDate", new Date())
+                .field("message", "trying out Elasticsearch")
+                .endObject();
+
+        IndexResponse response = client.prepareIndex("twitter", "tweet", "1")
+                .setSource(jsonBuilder()
+                        .startObject()
+                        .field("user", "kimchy")
+                        .field("postDate", new Date())
+                        .field("message", "trying out Elasticsearch")
+                        .endObject()
+                )
+                .get();
+
+
+        client.close();
+    }
+}
