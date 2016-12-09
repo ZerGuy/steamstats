64129a2f70bc90583213f2fd008cce9423db4d5a
Sergei Olenev <sergei_olenev@epam.com>
2016-12-09 18:43:22 +0300

    fix in location convertion

--- src/main/java/ru/zerguy/steamstats/LocationConverter.java
+++ src/main/java/ru/zerguy/steamstats/LocationConverter.java
@@ -1,7 +1,6 @@
 package ru.zerguy.steamstats;
 
 import org.apache.commons.io.IOUtils;
-import org.elasticsearch.client.transport.TransportClient;
 import org.json.simple.JSONObject;
 
 import java.io.IOException;
@@ -14,9 +13,8 @@ public class LocationConverter {
     public static final String LOCATION = "location";
     public static final String COORDINATES = "coordinates";
 
-    public final JSONObject id2City = loadCitiesMap();
+    public final JSONObject id2Country = loadCitiesMap();
 
-    private final TransportClient client = ElasticSearchConnectionFactory.createConnection();
     private final String CITIES_MAP_FILE = "steam_countries.json";
 
     public JSONObject loadCitiesMap() {
@@ -33,22 +31,39 @@ public class LocationConverter {
     }
 
     public void addLocation(JSONObject user) {
-        JSONObject county = (JSONObject) id2City.get(user.get(COUNTRY));
+        // Country
+        JSONObject county = (JSONObject) id2Country.get(String.valueOf(user.get(COUNTRY)));
         if (county == null)
             return;
 
-        JSONObject state = (JSONObject) id2City.get(user.get(STATE));
+        user.put("country", county.get("name"));
+
+
+        // State
+        JSONObject id2State = (JSONObject) county.get("states");
+        if (id2State == null)
+            return;
+
+        JSONObject state = (JSONObject) id2State.get(String.valueOf(user.get(STATE)));
         if (state == null) {
             user.put(LOCATION, county.get(COORDINATES));
             return;
         }
 
-        JSONObject city = (JSONObject) id2City.get(user.get(CITY));
+        user.put("state", state.get("name"));
+
+        // City
+        JSONObject id2City = (JSONObject) state.get("cities");
+        if (id2City == null)
+            return;
+
+        JSONObject city = (JSONObject) id2City.get(String.valueOf(user.get(CITY)));
         if (city == null) {
             user.put(LOCATION, state.get(COORDINATES));
             return;
         }
 
+        user.put("city", city.get("name"));
         user.put(LOCATION, city.get(COORDINATES));
     }
 
